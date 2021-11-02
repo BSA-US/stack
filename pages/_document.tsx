dd786b7543936404b9548e803b31d997eb3c0d33
replygirl <moon@moon.fyi>
2021-11-02 09:25:25 -0400

    favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,6 +9,27 @@ class StackDocument extends Document {
             href='https://fonts.googleapis.com/css2?family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap'
             rel='stylesheet'
           />
+          <link
+            rel='apple-touch-icon'
+            sizes='180x180'
+            href='/apple-touch-icon.png'
+          />
+          <link
+            rel='icon'
+            type='image/png'
+            sizes='32x32'
+            href='/favicon-32x32.png'
+          />
+          <link
+            rel='icon'
+            type='image/png'
+            sizes='16x16'
+            href='/favicon-16x16.png'
+          />
+          <link rel='manifest' href='/site.webmanifest' />
+          <link rel='mask-icon' href='/safari-pinned-tab.svg' color='#000000' />
+          <meta name='msapplication-TileColor' content='#000000' />
+          <meta name='theme-color' content='#ffffff' />
         </Head>
         <body>
           <Main />
