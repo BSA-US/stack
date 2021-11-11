7b758f7216e54be79e2a2d2afb6a36b52e8f8313
replygirl <moon@moon.fyi>
2021-11-11 15:16:36 -0500

    chore: lint

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const WindiCSSWebpackPlugin = require('windicss-webpack-plugin')
 
 module.exports = {
   webpack: config => {
-    config.plugins.push(new WindiCSSWebpackPlugin)
+    config.plugins.push(new WindiCSSWebpackPlugin())
     return config
   },
 }
